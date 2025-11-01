da29502eaa1e48fd8eec7d3f05790cf7481b8dd3
emann-o <eo180@student.le.ac.uk>
2025-11-01 14:15:37 +0000

    Create sections table in schooldatabase

---
+++ schooldatabase/sections.sql
@@ -0,0 +1,10 @@
+USE schooldatabase;
+
+CREATE TABLE sections(
+  section_ID INTEGER(6),
+  classroom VARCHAR(20),
+  PRIMARY KEY(section_ID)
+  );
+
+INSERT INTO sections VALUES(12345, 'Sir Bob Brugges');
+
