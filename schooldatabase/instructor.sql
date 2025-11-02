eb5702f578d7fe1c8bbcd5936e2f65dd8d5d5a56
Abbas <abbasjafari122005@gmail.com>
2025-11-02 20:51:11 +0000

    Update instructor.sql

--- schooldatabase/instructor.sql
+++ schooldatabase/instructor.sql
@@ -1,6 +1,6 @@
 use schooldatabase;
 create TABLE instructor (
-instructor_ID INT AUTO_INCREMENT PRIMARY KEY,
+instructor_id INT AUTO_INCREMENT PRIMARY KEY,
 full_name VARCHAR(100) NOT NULL,
 annual_salary DECIMAL(10,2) NOT NULL,
 dept_id INT NOT NULL,
@@ -8,5 +8,5 @@ FOREIGN KEY (dept_id) REFERENCES
 department(dept_id)
 );
 
-Insert into instructor (intructor_ID, full_name, annual_salary, dept_id)
+Insert into instructor (intructor_id, full_name, annual_salary, dept_id)
 VALUES ('1123456', 'john_doe', '100,000', 'Science');
