d236059701a4d7430e31fbb1c26ebed0e4140d49
Abbas <abbasjafari122005@gmail.com>
2025-11-02 15:40:09 +0000

    Update timeslot.sql

--- schooldatabase/timeslot.sql
+++ schooldatabase/timeslot.sql
@@ -2,10 +2,11 @@ use schooldatabase;
 
 CREATE TABLE timeslot(
 time_ID INTEGER, 
-day_slot VARCHAR(20)
-start_time TIME,
-end_time TIME
+day_slot VARCHAR(16) NOT NULL,
+start_time TIME NOT NULL,
+end_time TIME NOT NULL
 PRIMARY KEY (time_ID)
+CHECK (end_time> start_time)
 );
 
 insert into timeslot values (1, 'monday', '10:00:00', '12:00:00')
