ea2a369e4f03f4d63f0fb655f6d744c1ef5b7e40
Abbas <abbasjafari122005@gmail.com>
2025-11-02 20:50:54 +0000

    Create teaches.sql

---
+++ schooldatabase/teaches.sql
@@ -0,0 +1,16 @@
+use schooldatabase
+
+create table teaches (
+  teaches_id INT AUTO_INCREMENT PRIMARY KEY,
+  instructor_id INT NOT NULL,
+  section_id INT NOT NULL,
+
+CONSTRAINT fk_teaches_instructor
+  FOREIGN KEY (instructor_id) REFERENCES instructor(instructor_id)
+  CONSTRAINT fk_teaches_section
+  FOREIGN KEY (section_id) REFERENCES section(section_id),
+
+UNIQUE KEY uq_teaches (instructor_id, section_id)
+
+insert into teaches (instructor_id, section_id)
+VALUES (1, 1);
