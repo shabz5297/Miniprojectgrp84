415507ab9a3929e8735f9851fda79144ed46f829
Abbas <abbasjafari122005@gmail.com>
2025-11-02 21:09:05 +0000

    Update sections.sql

--- schooldatabase/sections.sql
+++ schooldatabase/sections.sql
@@ -2,15 +2,15 @@ USE schooldatabase;
 
 CREATE TABLE sections(
   section_id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  classroom_id INT NOT NULL,
-  course_id INT NOT NULL,
-  time_id INTEGER NOT NULL
+  classroom_id INTEGER NOT NULL,
+  course_id INTEGER NOT NULL,
+  time_id INTEGER NOT NULL,
   FOREIGN KEY (time_id) REFERENCES timeslot(time_id)
   FOREIGN KEY (course_id) REFERENCES courses(course_id)
-  FOREIN KEY (classroom_id) REFERENCES classroom(classroom_id)
+  FOREIGN KEY (classroom_id) REFERENCES classroom(classroom_id)
   
   );
 
-INSERT INTO sections (section_id, classroom, time_id
-VALUES(12345, 'Sir Bob Brugges', 1);
+INSERT INTO sections (section_id, classroom_id, time_id
+VALUES(1, 1, 1);
 
