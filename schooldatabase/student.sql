cbe6fb1e16805b40ef80e99c3f6dbd981e717df6
emann-o <eo180@student.le.ac.uk>
2025-11-02 19:43:26 +0000

    modified table from abbas by adding the foreign keys

--- schooldatabase/student.sql
+++ schooldatabase/student.sql
@@ -1,10 +1,12 @@
 use schooldatabase;
 CREATE table student (
-student_ID INTEGER(6),
+student_ID INTEGER PRIMARY KEY,
 full_name VARCHAR(20),
-total_credits INTEGER(3),
-enrolled_course VARCHAR(20)
+total_credits INTEGER,
+enrolled_course VARCHAR(20),
+course_id Integer,
+FOREIGN KEY(course_id) REFERENCES courses(course_id)
 );
 
 insert into student VALUES
-('123456', 'jane_doe', '120', 'English');
+('123456', 'jane_doe', '120', 'English', 123456);
