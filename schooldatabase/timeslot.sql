25eafb38312a3b4aeb2b87c473e472bf4878c758
Abbas <abbasjafari122005@gmail.com>
2025-11-02 15:40:21 +0000

    Update timeslot.sql

--- schooldatabase/timeslot.sql
+++ schooldatabase/timeslot.sql
@@ -6,7 +6,7 @@ day_slot VARCHAR(16) NOT NULL,
 start_time TIME NOT NULL,
 end_time TIME NOT NULL
 PRIMARY KEY (time_ID)
-CHECK (end_time> start_time)
+CHECK (end_time > start_time)
 );
 
 insert into timeslot values (1, 'monday', '10:00:00', '12:00:00')
