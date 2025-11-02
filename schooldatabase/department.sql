13e048b05dde2f91b4e994660528da8b34394f1a
Abbas <abbasjafari122005@gmail.com>
2025-11-02 17:51:44 +0000

    Update department.sql

--- schooldatabase/department.sql
+++ schooldatabase/department.sql
@@ -1,10 +1,10 @@
 use schooldatabase;
 CREATE TABLE department (
-dept_name VARCHAR(20),
-location VARCHAR(20),
-annual_budget INTEGER(6),
-PRIMARY KEY (dept_name)
+dept_id INT AUTO_INCREMENT PRIMARY KEY,
+dept_name VARCHAR(20) UNIQUE NOT NULL,
+location VARCHAR(20) NOT NULL,
+annual_budget INTEGER(6) NOT NULL,
 );
 
 Insert into department values 
-('Computing', 'South' '100,000');
+('Computing', 'South' , '100000');
