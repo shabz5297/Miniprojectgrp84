8f394fc928198a555a328b4c92462b45098c5d03
Abbas <abbasjafari122005@gmail.com>
2025-11-02 19:42:26 +0000

    Update courses.sql

--- schooldatabase/courses.sql
+++ schooldatabase/courses.sql
@@ -1,11 +1,15 @@
 use schooldatabase;
 
 CREATE TABLE courses(
-  course_ID INTEGER(6),
-  course_title VARCHAR(20),
-  credit_value INTEGER(3),
-  sections VARCHAR(20),
-  PRIMARY KEY(course_ID)
+  course_ID INTEGER AUTO_INCREMENT PRIMARY KEY,
+  course_title VARCHAR(100) NOT NULL,
+  credit_value INTEGER NOT NULL CHECK(credit_value > 0)
+  dept_id INTEGER NOT NULL,
+  CONSTRAINT fk_courses_department
+  FOREIGN KEY (dept_id) REFERENCES
+  ON UPDA
+  department(dept_id)
   );
   
-insert into courses values(123456, 'Software', 20, 'Architecture');
+insert into courses (course_title, credit_value, dept_id)
+VALUES  ('software', 20, 1);
