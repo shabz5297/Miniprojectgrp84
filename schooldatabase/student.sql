d2a43a956bdf369cbf88794694de27e0feeb2ecd
emann-o <eo180@student.le.ac.uk>
2025-11-03 14:48:34 +0000

    Fix course_id reference in student table insert

--- schooldatabase/student.sql
+++ schooldatabase/student.sql
@@ -3,10 +3,9 @@ CREATE table student (
 student_id INTEGER PRIMARY KEY,
 full_name VARCHAR(20),
 total_credits INTEGER,
-enrolled_course VARCHAR(20),
 course_id Integer,
 FOREIGN KEY(course_id) REFERENCES courses(course_id)
 );
 
 insert into student VALUES
-('123456', 'jane_doe', '120', 'English', 123456);
+('123456', 'jane_doe', '120', 'English', 1);
