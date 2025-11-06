2d06a1eb810aa7c2c67fb2f7667a3ae2214abef0
emann-o <eo180@student.le.ac.uk>
2025-11-06 00:35:04 +0000

    Modify sections table structure and insert statement eman
    Updated sections table to include term and adjusted primary key.

--- schooldatabase/sections.sql
+++ schooldatabase/sections.sql
@@ -1,16 +1,18 @@
 USE schooldatabase;
 
 CREATE TABLE sections(
-  section_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  section_id INTEGER NOT NULL,
   classroom_id INTEGER NOT NULL,
   course_id INTEGER NOT NULL,
   time_id INTEGER NOT NULL,
-  FOREIGN KEY (time_id) REFERENCES timeslot(time_id)
-  FOREIGN KEY (course_id) REFERENCES courses(course_id)
-  FOREIGN KEY (classroom_id) REFERENCES classroom(classroom_id)
+  term VARCHAR(30) NOT NULL,
+  FOREIGN KEY (time_id) REFERENCES timeslot(time_id),
+  FOREIGN KEY (course_id) REFERENCES courses(course_id),
+  FOREIGN KEY (classroom_id) REFERENCES classroom(classroom_id),
+  PRIMARY KEY(course_id, term)
   
   );
 
-INSERT INTO sections (section_id, classroom_id, time_id
-VALUES(1, 1, 1);
+INSERT INTO sections (section_id, classroom_id, time_id, course_id, term)
+VALUES(1, 1, 1, 1, "summer");
 
